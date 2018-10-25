6682f1a52726dfd53d37523658f9043e89671481
akintos <akintos47@gmail.com>
2018-10-26 02:18:29 +0900

    Fix save/load errors

--- CompanionCustomPortrait/Patch.cs
+++ CompanionCustomPortrait/Patch.cs
@@ -1,11 +1,15 @@
 ﻿using Harmony12;
 using Kingmaker.Blueprints;
 using Kingmaker.EntitySystem.Entities;
+using Kingmaker.EntitySystem.Persistence;
+using Kingmaker.UI.SaveLoadWindow;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace CompanionCustomPortrait
 {
@@ -50,6 +54,56 @@ namespace CompanionCustomPortrait
             }
 
             return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(SaveManager), "ExtractPortrait")]
+    public static class ExtractPortrait_Patch
+    {
+        public static bool Prefix(SaveManager __instance, ref PortraitForSave __result, UnitEntityData u)
+        {
+            var name = u.UISettings.Owner.Blueprint.LocalizedName;
+            if (name && CompanionCustomPortraitsManager.Instance.GetPortrait(name.String.Key))
+            {
+                var portrait = (BlueprintPortrait)typeof(BlueprintUnit).GetField("m_Portrait", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(u.UISettings.Owner.Blueprint);
+                __result = new PortraitForSave(portrait);
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(SaveLoadPortraits), "Set")]
+    public static class SaveLoadPortraits_Patch
+    {
+        public static bool Prefix(SaveLoadPortraits __instance, SaveInfo saveInfo, ref List<SaveLoadPortait> ___m_Portraits)
+        {
+            try
+            {
+                __instance.Reset();
+                if (saveInfo == null)
+                {
+                    return false;
+                }
+                List<Sprite> list = new List<Sprite>();
+                if (saveInfo.PartyPortraits != null)
+                {
+                    list.AddRange(from companions in saveInfo.PartyPortraits
+                                  where companions != null && companions.Data != null
+                                  select companions.Data.SmallPortrait);
+                }
+                for (int i = 0; i < ___m_Portraits.Count; i++)
+                {
+                    ___m_Portraits[i].Set((list.Count <= i) ? null : list[i]);
+                }
+            }
+            catch (Exception e)
+            {
+                FileLog.Log(e.ToString());
+            }
+            
+            return false;
         }
     }
 }
